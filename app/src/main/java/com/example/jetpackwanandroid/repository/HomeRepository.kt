9d14c270e00006707f83358288200c4045a451a9
霍振鹏 <huozhenpengdyx@gmail.com>
2019-12-09 10:23:53 +0800

    修改初始加载全部的bug

--- app/src/main/java/com/example/jetpackwanandroid/repository/HomeRepository.kt
+++ app/src/main/java/com/example/jetpackwanandroid/repository/HomeRepository.kt
@@ -3,6 +3,8 @@ package com.example.jetpackwanandroid.repository
 import android.app.Application
 import androidx.annotation.MainThread
 import androidx.paging.DataSource
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
 import androidx.paging.toLiveData
 import com.example.jetpackwanandroid.bean.ArticleItemData
 import com.example.jetpackwanandroid.bean.ArticleResult
@@ -24,10 +26,20 @@ class HomeRepository(
             handleResponse = this::insertResultIntoDb
         )
 
-        val livePagedList= dao.getAll().toLiveData(
-            pageSize=10,
-            boundaryCallback = boundaryCallback
-        )
+        //初始会加载全部，没找到原因，改用下面的方式
+//        val livePagedList= dao.getAll().toLiveData(
+//            pageSize=10,
+//            boundaryCallback = boundaryCallback
+//        )
+
+        val config = PagedList.Config.Builder()
+            .setPageSize(15)              // 分页加载的数量
+            .setInitialLoadSizeHint(30)   // 初次加载的数量
+            .setPrefetchDistance(10)      // 预取数据的距离
+            .setEnablePlaceholders(false) // 是否启用占位符
+            .build()
+
+        val livePagedList=LivePagedListBuilder(dao.getAll(),config).build()
 
         return Listing(
             pagedList = livePagedList
